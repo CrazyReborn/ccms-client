85c492a0c51eb2bab5b598a525a08a23b349e28c
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-08-22 18:30:29 +0200

    fix: now can assign users on task creation. also add a validation to locaiton (not very though).

--- src/components/TaskCreateForm.tsx
+++ src/components/TaskCreateForm.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useState } from 'react';
+import React, { SyntheticEvent, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import LoadingSpinner from './LoadingSpinnner';
 import CloseIcon from '../images/icons/close_FILL0_wght400_GRAD0_opsz48.png';
@@ -24,25 +24,49 @@ function TargetLocation(props: any) {
 }
 
 export const TaskCreateForm = (props: any) => {
-  const [assignedTo, setAssignedTo] = useState(null);
+  const [assignedTo, setAssignedTo] = useState('');
   const [date, setDate] = useState('');
   const [description, setDescription] = useState('');
   const [name, setName] = useState('');
+  const [users, setUsers] = useState([]);
   const [location, setLocation] = useState([0, 0]);
   const token = localStorage.getItem('access_token');
   const { showCreateTaskForm, setShowCreateTaskForm } = props;
   const setParentLoaded = props.setLoaded;
-  const [loaded, setLoaded] = useState(true);
+  const [loaded, setLoaded] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!loaded && showCreateTaskForm) {
+      //fetch users for the thing
+      fetch(`${process.env.REACT_APP_SERVER_URL}/users`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => setUsers(json))
+      .catch((err) => console.log(err))
+      .finally(() => setLoaded(true));
+    }
+  }, [showCreateTaskForm])
 
   function onSubmit(e: SyntheticEvent) {
     e.preventDefault();
+    if(location[0] == 0 || location[1] == 1) {
+      throw new Error('wrong location');
+    }
     sendForm();
   }
 
   function sendForm() {
     const body = {
-      assignedTo,
+      assignedTo: assignedTo === '' ? null : assignedTo,
       date,
       description,
       name,
@@ -68,7 +92,7 @@ export const TaskCreateForm = (props: any) => {
   }
 
   if(!showCreateTaskForm) return null;
-  if(!loaded) {
+  if(!loaded && showCreateTaskForm) {
     return ReactDOM.createPortal(
       <LoadingSpinner />
       ,
@@ -86,15 +110,28 @@ export const TaskCreateForm = (props: any) => {
         </div>
         <label htmlFor='name'>
           Name
-          <input type='text' onChange={(e) => setName(e.target.value)}/>
+          <input required={true} type='text' onChange={(e) => setName(e.target.value)}/>
         </label>
-        <label htmlFor='date'>
-          Date
-          <input type='date' onChange={(e) => { setDate(e.target.value) }}/>
-        </label>
+        <div className='controls'>
+          <label htmlFor='date'>
+            Date
+            <input required={true} type='date' onChange={(e) => { setDate(e.target.value) }}/>
+          </label>
+          <label htmlFor='users'>
+            Assign to
+            <select id='users' onChange={(e) => setAssignedTo(e.target.value)}>
+              <option value={''}>Nobody</option>
+              {users.map((user) => {
+                return (
+                  <option key={user['_id']} value={user['_id']}>{user['firstName']} {user['lastName']}</option>
+                )
+              })}
+            </select>
+          </label>
+        </div>
         <label htmlFor='name'>
           Description
-          <textarea value={description} rows={5} onChange={(e) => setDescription(e.target.value)}/>
+          <textarea required={true} value={description} rows={5} onChange={(e) => setDescription(e.target.value)}/>
         </label>
         <div className='map-selector'>
           <h5>Click on the map to select the location</h5>
